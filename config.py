a292a8d3ab8be689763b49a2bf87656243bc3dde
naruto1427 <pritnaruto@gmail.com>
2025-05-18 04:03:50 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,201 @@
+"""
+Apache License 2.0
+Copyright (c) 2022 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+import re, os, time
+id_pattern = re.compile(r'^.\d+$') 
+
+class Config(object):
+    # digital_botz client config
+    API_ID = os.environ.get("API_ID", "")
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+    # premium account string session required 😢 
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    
+    # database config
+    DB_NAME = os.environ.get("DB_NAME","Rename db")     
+    DB_URL = os.environ.get("DB_URL","")
+ 
+    # other configs
+    RKN_PIC = os.environ.get("RKN_PIC", "https://envs.sh/s/VIiLCaeJ8f9W-BZvQxGVpQ/rdl.jpg")
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7984561417, 6672752177').split()]
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002585613766"))
+
+    # free upload limit 
+    FREE_UPLOAD_LIMIT = 6442466 # calculation 6*1024*1024*1024=results
+
+    # premium mode feature ✅
+    UPLOAD_LIMIT_MODE = True 
+    PREMIUM_MODE = True 
+    
+    #force subs
+    try:
+        FORCE_SUB = int(os.environ.get("FORCE_SUB", "")) 
+    except:
+        FORCE_SUB = os.environ.get("FORCE_SUB", "")
+        
+    # wes response configuration     
+    PORT = int(os.environ.get("PORT", "8080"))
+    BOT_UPTIME = time.time()
+
+class rkn(object):
+    # part of text configuration
+    START_TXT = """<b>Ｈ𝙰𝙸, {}👋
+
+𝚃ʜɪs 𝙸s 𝙰ɴ 𝙰ᴅᴠᴀᴄᴇᴅ 𝙰ɴᴅ 𝚈ᴇᴛ 𝙿ᴏᴡᴇʀғᴜʟ 𝚁ᴇɴᴀᴍᴇ 𝙱ᴏᴛ
+</b>"""
+
+    ABOUT_TXT = """<b>╭───────────⍟
+├🤖 ᴍy ɴᴀᴍᴇ : {}
+├🖥️ Dᴇᴠᴇʟᴏᴩᴇʀꜱ : {}
+├👨‍💻 Pʀᴏɢʀᴀᴍᴇʀ : {}
+├📕 Lɪʙʀᴀʀy : {}
+├✏️ Lᴀɴɢᴜᴀɢᴇ: {}
+├💾 Dᴀᴛᴀ Bᴀꜱᴇ: {}
+├📊 ᴠᴇʀsɪᴏɴ: <a href=https://github.com/DigitalBotz/Digital-Rename-Bot>{}</a></b>     
+╰───────────────⍟ """
+
+    HELP_TXT = """
+<b>•></b> /start Tʜᴇ Bᴏᴛ.
+
+✏️ <b><u>Hᴏᴡ Tᴏ Rᴇɴᴀᴍᴇ A Fɪʟᴇ</u></b>
+<b>•></b> Sᴇɴᴅ Aɴy Fɪʟᴇ Aɴᴅ Tyᴩᴇ Nᴇᴡ Fɪʟᴇ Nɴᴀᴍᴇ \nAɴᴅ Aᴇʟᴇᴄᴛ Tʜᴇ Fᴏʀᴍᴀᴛ [ document, video, audio ].           
+"""
+
+    UPGRADE_PREMIUM= """
+•⪼ ★𝘗𝘭𝘢𝘯𝘴    -  ⏳𝘋𝘢𝘵𝘦 - 💸𝘗𝘳𝘪𝘤𝘦 
+•⪼ 🥉𝘉𝘳𝘰𝘯𝘻𝘦  -   3𝘥𝘢𝘺𝘴 -   10
+•⪼ 🥈𝘚𝘪𝘭𝘷𝘦𝘳   -   7𝘥𝘢𝘺𝘴 -   30
+•⪼ 🥇𝘎𝘰𝘭𝘥    -  15𝘥𝘢𝘺𝘴 -  50
+•⪼ 🏆𝘗𝘭𝘢𝘵𝘪𝘯𝘶𝘮 -  1𝘮𝘰𝘯𝘵𝘩 -  70
+•⪼ 💎𝘋𝘪𝘢𝘮𝘰𝘯𝘥 -  2𝘮𝘰𝘯𝘵𝘩 -  80
+
+- 𝘋𝘢𝘪𝘭𝘺 𝘜𝘱𝘭𝘰𝘢𝘥 𝘓𝘪𝘮𝘪𝘵 𝘜𝘯𝘭𝘪𝘮𝘪𝘵𝘦𝘥
+- 𝘋𝘪𝘴𝘤𝘰𝘶𝘯𝘵 𝘈𝘭𝘭 𝘗𝘭𝘢𝘯 𝘙𝘴.9
+    """
+    
+    UPGRADE_PLAN= """
+𝘗𝘭𝘢𝘯: 𝘗𝘳𝘰
+𝘋𝘢𝘵𝘦: 1 𝘮𝘰𝘯𝘵𝘩 
+𝘗𝘳𝘪𝘤𝘦: 70
+𝘓𝘪𝘮𝘪𝘵: 100 𝘎𝘉
+
+𝘗𝘭𝘢𝘯: 𝘜𝘭𝘵𝘢 𝘗𝘳𝘰 
+𝘋𝘢𝘵𝘦: 1 𝘮𝘰𝘯𝘵𝘩 
+𝘗𝘳𝘪𝘤𝘦: 80
+𝘓𝘪𝘮𝘪𝘵: 1000 𝘎𝘉
+
+- 𝘋𝘪𝘴𝘤𝘰𝘶𝘯𝘵 𝘈𝘭𝘭 𝘗𝘭𝘢𝘯 𝘙𝘴.9
+    """
+    
+    THUMBNAIL = """
+🌌 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ</u></b>
+
+<b>•></b> Sᴇɴᴅ Aɴy Pʜᴏᴛᴏ Tᴏ Aᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟy Sᴇᴛ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /del_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Oʟᴅ Tʜᴜᴍʙɴɪʟᴇ.
+<b>•></b> /view_thumb Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Tʜᴜᴍʙɴɪʟᴇ.
+"""
+    CAPTION= """
+📑 <b><u>Hᴏᴡ Tᴏ Sᴇᴛ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ</u></b>
+
+<b>•></b> /set_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Sᴇᴛ ᴀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /see_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Vɪᴇᴡ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+<b>•></b> /del_caption - Uꜱᴇ Tʜɪꜱ Cᴏᴍᴍᴀɴᴅ Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Cᴜꜱᴛᴏᴍ Cᴀᴩᴛɪᴏɴ
+
+Exᴀᴍᴩʟᴇ:- `/set_caption 📕 Fɪʟᴇ Nᴀᴍᴇ: {filename}
+💾 Sɪᴢᴇ: {filesize}
+⏰ Dᴜʀᴀᴛɪᴏɴ: {duration}`
+"""
+    BOT_STATUS = """
+⚡️ ʙᴏᴛ sᴛᴀᴛᴜs ⚡️
+
+⌚️ ʙᴏᴛ ᴜᴩᴛɪᴍᴇ: `{}`
+👭 ᴛᴏᴛᴀʟ ᴜsᴇʀꜱ: `{}`
+💸 ᴛᴏᴛᴀʟ ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀs: `{}`
+֍ ᴜᴘʟᴏᴀᴅ: `{}`
+⊙ ᴅᴏᴡɴʟᴏᴀᴅ: `{}`
+"""
+    LIVE_STATUS = """
+⚡ ʟɪᴠᴇ sᴇʀᴠᴇʀ sᴛᴀᴛᴜs ⚡
+
+ᴜᴘᴛɪᴍᴇ: `{}`
+ᴄᴘᴜ: `{}%`
+ʀᴀᴍ: `{}%` 
+ᴛᴏᴛᴀʟ ᴅɪsᴋ: `{}`
+ᴜsᴇᴅ sᴘᴀᴄᴇ: `{} {}%`
+ғʀᴇᴇ sᴘᴀᴄᴇ: `{}`
+ᴜᴘʟᴏᴀᴅ: `{}`
+ᴅᴏᴡɴʟᴏᴀᴅ: `{}`
+V𝟹.𝟶.𝟶 [STABLE]
+"""
+    DIGITAL_METADATA = """
+❪ SET CUSTOM METADATA ❫
+
+- /metadata - Tᴏ Sᴇᴛ & Cʜᴀɴɢᴇ ʏᴏᴜʀ ᴍᴇᴛᴀᴅᴀᴛᴀ ᴄᴏᴅᴇ
+
+☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
+
+`--change-title @
+--change-video-title @
+--change-audio-title @
+--change-subtitle-title @
+--change-author @`
+
+"""
+    
+    CUSTOM_FILE_NAME = """
+<u>🖋️ Custom File Name</u>
+
+you can pre-add a prefix and suffix along with your new filename
+
+➢ /set_prefix - To add a prefix along with your _filename.
+➢ /see_prefix - Tᴏ Sᴇᴇ Yᴏᴜʀ Pʀᴇғɪx !!
+➢ /del_prefix - Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Pʀᴇғɪx !!
+➢ /set_suffix - To add a suffix along with your filename_.
+➢ /see_suffix - Tᴏ Sᴇᴇ Yᴏᴜʀ Sᴜғғɪx !!
+➢ /del_suffix - Tᴏ Dᴇʟᴇᴛᴇ Yᴏᴜʀ Sᴜғғɪx !!
+
+Exᴀᴍᴩʟᴇ:- `/set_suffix @yourchannel`
+Exᴀᴍᴩʟᴇ:- `/set_prefix @yourchannel`
+"""
+    
+
+    SEND_METADATA = """
+❪ SET CUSTOM METADATA ❫
+
+☞ Fᴏʀ Exᴀᴍᴘʟᴇ:-
+
+`--change-title @yourchannel
+--change-video-title @yourchannel
+--change-audio-title @yourchannel
+--change-subtitle-title @yourchannel
+--change-author @yourchannel`
+ 
+"""
+    
+    RKN_PROGRESS = """<b>\n
+╭━━━━❰RKN PROCESSING...❱━➣
+┣⪼ 🗃️ ꜱɪᴢᴇ: {1} | {2}
+┣⪼ ⏳️ ᴅᴏɴᴇ : {0}%
+┣⪼ 🚀 ꜱᴩᴇᴇᴅ: {3}/s
+┣⪼ ⏰️ ᴇᴛᴀ: {4}
+╰━━━━━━━━━━━━━━━➣ </b>"""
+

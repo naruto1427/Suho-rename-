11b7d322109c891270698784ee1e8ba13f5864c9
naruto1427 <pritnaruto@gmail.com>
2025-05-18 04:39:27 +0530

    Add files via upload

---
+++ plugins/__init__.py
@@ -0,0 +1,59 @@
+
+"""
+Apache License 2.0
+Copyright (c) 2022 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+"""
+
+__name__ = "Suho-rename"
+__version__ = "3.0.8"
+__license__ = " Apache License, Version 2.0"
+__copyright__ = "Copyright (C) 2022-present Digital Botz <https://github.com/naruto1427>"
+__programer__ = "<a https://github.com/naruto1427>naruto</a>"
+__library__ = "<a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>"
+__language__ = "<a href=https://www.python.org/>Pyᴛʜᴏɴ 3</a>"
+__database__ = "<a href=https://cloud.mongodb.com/>Mᴏɴɢᴏ DB</a>"
+__developer__ = "<a https://github.com/naruto1427>naruto</a>"
+__maindeveloper__ = "<a https://github.com/naruto1427>naruti</a>"
+
+# main copyright herders (©️)
+# I have been working on this repo since 2022
+
+
+# main working files 
+# - bot.py
+# - web_support.py
+# - plugins/
+# - start_&_cb.py
+# - Force_Sub.py
+# - admin_panel.py
+# - file_rename.py
+# - metadata.py
+# - prefix_&_suffix.py
+# - thumb_&_cap.py
+# - config.py
+# - utils.py
+# - database.py
+
+# bot run files
+# - bot.py
+# - Procfile
+# - Dockerfile
+# - requirements.txt
+# - runtime.txt
---
+++ plugins/prefix_&_suffix.py
@@ -0,0 +1,81 @@
+
+"""
+Apache License 2.0
+Copyright (c) 2022 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+# imports
+from pyrogram import Client, filters, enums
+from helper.database import digital_botz
+
+# prefix commond ✨
+@Client.on_message(filters.private & filters.command('set_prefix'))
+async def add_prefix(client, message):
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Prefix__\n\nExᴀᴍᴩʟᴇ:- `/set_prefix @yourchannel`**")
+    prefix = message.text.split(" ", 1)[1]
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await digital_botz.set_prefix(message.from_user.id, prefix)
+    await RknDev.edit("__**✅ ᴘʀᴇꜰɪx ꜱᴀᴠᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('del_prefix'))
+async def delete_prefix(client, message):
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    prefix = await digital_botz.get_prefix(message.from_user.id)
+    if not prefix:
+        return await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+    await digital_botz.set_prefix(message.from_user.id, None)
+    await RknDev.edit("__**❌️ ᴘʀᴇꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('see_prefix'))
+async def see_prefix(client, message):
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    prefix = await digital_botz.get_prefix(message.from_user.id)
+    if prefix:
+        await RknDev.edit(f"**ʏᴏᴜʀ ᴘʀᴇꜰɪx:-**\n\n`{prefix}`")
+    else:
+        await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+
+# SUFFIX COMMOND ✨
+@Client.on_message(filters.private & filters.command('set_suffix'))
+async def add_suffix(client, message):
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Suffix__\n\nExᴀᴍᴩʟᴇ:- `/set_suffix @yourchannel`**")
+    suffix = message.text.split(" ", 1)[1]
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await digital_botz.set_suffix(message.from_user.id, suffix)
+    await RknDev.edit("__**✅ ꜱᴜꜰꜰɪx ꜱᴀᴠᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('del_suffix'))
+async def delete_suffix(client, message):
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    suffix = await digital_botz.get_suffix(message.from_user.id)
+    if not suffix:
+        return await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
+    await digital_botz.set_suffix(message.from_user.id, None)
+    await RknDev.edit("__**❌️ ꜱᴜꜰꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('see_suffix'))
+async def see_suffix(client, message):
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    suffix = await digital_botz.get_suffix(message.from_user.id)
+    if suffix:
+        await RknDev.edit(f"**ʏᴏᴜʀ ꜱᴜꜰꜰɪx:-**\n\n`{suffix}`")
+    else:
+        await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
